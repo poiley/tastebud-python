f403716e85ef4dd6d58e414bf861231db962694b
Benjamin Poile <benp51@outlook.com>
2018-02-09 09:19:01 -0800

    Cleaned code, updating DB and going to implement logging soon.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import requests, json, time, os, datetime
 import spotifyAuth, spotifyCalculations
 
+URL_BASE = "https://api.spotify.com/v1/"
+
 class Bunch:
     def __init__(self, **kwds):
         self.__dict__.update(kwds)
 
 def read_data(headers, params):
-    response = requests.get("https://api.spotify.com/v1/me/player/currently-playing", headers=headers, params=params)
+    url = URL_BASE + "me/player/currently-playing"
+    response = requests.get(url, headers=headers, params=params)
 
     response_as_text = "{}"
     if response.status_code != 204:
@@ -15,15 +18,18 @@ def read_data(headers, params):
     return response.status_code, json.loads(response_as_text)
 
 def read_artist_data(headers, params, uri):
-    response = requests.get("https://api.spotify.com/v1/artists/" + uri, headers=headers, params=params)
+    url = URL_BASE + "artists/" + uri
+    response = requests.get(url, headers=headers, params=params)
     return json.loads(response.text)
 
 def read_playlist_data(headers, params, user_id, playlist_id):
-    response = requests.get("https://api.spotify.com/v1/users/" + user_id +"/playlists/" + playlist_id, headers=headers, params=params)
+    url = URL_BASE + "users/" + user_id +"/playlists/" + playlist_id
+    response = requests.get(url, headers=headers, params=params)
     return json.loads(response.text)
 
 def read_audio_analysis_data(headers, params, uri):
-    response = requests.get("https://api.spotify.com/v1/audio-features/" + uri, headers=headers, params=params)
+    url = URL_BASE + "audio-features/" + uri
+    response = requests.get(url, headers=headers, params=params)
     return json.loads(response.text)
 
 def write_data(now_playing, verbose=False):
@@ -41,7 +47,7 @@ def write_data(now_playing, verbose=False):
                 str(now_playing.start_time) + "," + \
                 str(now_playing.tempo)
 
-    if now_playing.song != get_last_line().split(",")[0]: #if the latest song in the list is NOT the one playing
+    if now_playing.song != get_last_line().split(",")[0]:
         open("data.csv", "a").write("\n"+output+"\n")
     else:
         lines = open("data.csv", "r").readlines()
@@ -71,35 +77,46 @@ def sanitize_data():
 def get_song_data(data, headers=None, params=None):
     now_playing = Bunch(playlist="", genre = "", tempo = -1.0)
 
-    now_playing.song        = str(data["item"]["name"]).replace(",", "")
-    now_playing.album       = str(data["item"]["album"]["name"]).replace(",", "")
-    now_playing.artist      = str(data["item"]["artists"][0]["name"]).replace(",", "")
-    now_playing.uri         = data["item"]["uri"]
-    now_playing.length      = data["item"]["duration_ms"]
-    now_playing.progress    = data["progress_ms"]
+    now_playing.song     = str(data["item"]["name"]).replace(",", "")
+    now_playing.album    = str(data["item"]["album"]["name"]).replace(",", "")
+    now_playing.artist   = str(data["item"]["artists"][0]["name"]).replace(",", "")
+    now_playing.uri      = data["item"]["uri"]
+    now_playing.length   = data["item"]["duration_ms"]
+    now_playing.progress = data["progress_ms"]
 
-    start_time              = datetime.datetime.now() - datetime.timedelta(milliseconds=now_playing.progress)
+    start_time           = datetime.datetime.now() - datetime.timedelta(milliseconds=now_playing.progress)
     now_playing.start_time  = start_time.strftime("%w %H:%M:%S")
 
     if headers is not None and params is not None:
         try:
-            user_id              = data["context"]["uri"].split(":")[2]
-            playlist_id          = data["context"]["uri"].split(":")[4]
-            playlist_data        = read_playlist_data(headers, params, user_id, playlist_id)
-            now_playing.playlist = playlist_data["name"].replace(",", "")
+            if data["context"]["type"] == "playlist":
+                user_id              = data["context"]["uri"].split(":")[2]
+                playlist_id          = data["context"]["uri"].split(":")[4]
+                playlist_data        = read_playlist_data(headers,
+                                                          params,
+                                                          user_id,
+                                                          playlist_id)
+
+                now_playing.playlist = playlist_data["name"].replace(",", "")
         except (KeyError, TypeError):
             pass
 
         try:
             song_id              = now_playing.uri.split(":")[2]
-            audio_analysis_data  = read_audio_analysis_data(headers, params, song_id)
+            audio_analysis_data  = read_audio_analysis_data(headers,
+                                                            params,
+                                                            song_id)
+
             now_playing.tempo    = float(audio_analysis_data["tempo"])
         except (KeyError, TypeError):
             pass
 
         try:
             artist_id            = data["item"]["artists"][0]["id"]
-            artist_data          = read_artist_data(headers, params, artist_id)
+            artist_data          = read_artist_data(headers,
+                                                    params,
+                                                    artist_id)
+
             now_playing.genre    = artist_data["genres"][0].replace(",", "")
         except (KeyError, TypeError):
             pass
@@ -116,10 +133,11 @@ def print_album(now_playing):
     side_lines = "|"+ (20 * " ") +"|"
     base       = "|     ;          ;   |\t"
 
-    song    = "Song:\t\t"  + ((now_playing.song[:27]   + '...') if len(now_playing.song)   >= 27 else now_playing.song)
-    album   = "Album:\t\t" + ((now_playing.album[:27]  + '...') if len(now_playing.album)  >= 27 else now_playing.album)
-    artist  = "Artist\t\t" + ((now_playing.artist[:27] + '...') if len(now_playing.artist) >= 27 else now_playing.artist)
+    song    = "Song:\t\t"   + ((now_playing.song[:27]     + '...') if len(now_playing.song)     >= 27 else now_playing.song)
+    album   = "Album:\t\t"  + ((now_playing.album[:27]    + '...') if len(now_playing.album)    >= 27 else now_playing.album)
+    artist  = "Artist\t\t"  + ((now_playing.artist[:27]   + '...') if len(now_playing.artist)   >= 27 else now_playing.artist)
     playlist= "Playlist:\t" + ((now_playing.playlist[:27] + '...') if len(now_playing.playlist) >= 27 else now_playing.playlist)
+
     print(" " + (20 * "_") + " \n" + side_lines)
     print("|     ;;;;;;;;;;;;   |")
     print(base + song + "\n" + base + album + "\n" + base + artist + "\n" + base + playlist + "\n" + base + "\n" + base)
@@ -132,8 +150,13 @@ def get_token():
     auth = json.load(open("authentication.json", 'r'))
     return auth["user-token"]
 
+def get_header():
+    return { 'Accept': 'application/json',
+             'Content-Type': 'application/json',
+             'Authorization': 'Bearer '+get_token(), }
+
 def main():
-    headers     = { 'Accept': 'application/json', 'Content-Type': 'application/json', 'Authorization': 'Bearer '+get_token(), }
+    headers     = get_header()
     params      = ( ('market', 'US'), )
 
     code, data = read_data(headers, params)
@@ -145,13 +168,13 @@ def main():
         while(True):
             code, data = read_data(headers, params)
 
-            if code == 401:
-                headers = { 'Accept': 'application/json', 'Content-Type': 'application/json', 'Authorization': 'Bearer '+get_token(), }
-                continue
-            elif code == 204: # nothing playing!
-                os.system("cls")
-                print("Nothing Playing.")
-                time.sleep(1)
+            if code != 200:
+                if code == 401:
+                    headers = get_header()
+                elif code == 204: # nothing playing!
+                    os.system("cls")
+                    print("Nothing Playing.")
+                    time.sleep(1)
                 continue
 
             now_playing = get_song_data(data, headers, params)
